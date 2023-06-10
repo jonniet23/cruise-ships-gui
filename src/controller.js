35dd3f489765c3b1b0dfac4f8a0f2cacebafde24
Jonnie <jonniet23@hotmail.com>
2023-06-10 20:17:39 +0100

    added itinerary and ports stage 9

--- src/controller.js
+++ src/controller.js
@@ -14,6 +14,22 @@ Controller.prototype.initialiseSea = function initialiseSea(){
       backgroundIndex += 1;
      }, 1000);
  };
+
+ Controller.prototype.renderPorts = function renderPorts(ports){
+    { const portsElement = document.querySelector('#ports');
+    portsElement.style.width = '0px';
+    ports.forEach((port, index) => {
+        const newPortElement = document.createElement('div');
+        newPortElement.className = 'port';
+        newPortElement.dataset.portName = port.name;
+        newPortElement.dataset.portIndex = index;
+      
+        portsElement.appendChild(newPortElement);
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`;
+      })}
+    };
+  
  if (typeof module !== 'undefined' && module.exports) {
     module.exports = Controller;
 } else {
